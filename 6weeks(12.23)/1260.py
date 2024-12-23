4d90e2e7fa6ad1860ff18632e1a78943762d896d
곽수찬 <nowee018@naver.com>
2024-12-23 13:47:38 +0900

    dfs, bfs

---
+++ 6weeks(12.23)/1260.py
@@ -0,0 +1,44 @@
+import sys
+# 입력값 초기화
+input = sys.stdin.readline
+N, M, V = map(int, input().split())  # 4 5 1 정점의 개수, 간선의 개수, 시작할 번호
+
+
+def dfs(idx):
+    global visited
+    visited[idx] = True
+    print(idx, end=" ")
+    for next in range(1, N+1):
+        if not visited[next] and graph[idx][next]:
+            dfs(next)
+
+
+def bfs():
+    global q, visited
+    while q:
+        cur = q.pop(0)
+        visited[cur] = True
+        print(cur, end=' ')
+        for next in range(1, N+1):
+            if not visited[next] and graph[cur][next]:
+                visited[next] = True
+                q.append(next)
+
+
+graph = [[False] * (N+1) for _ in range(N+1)]  # 정점의 개수로,,,,
+visited = [False] * (N+1)
+
+# 1. graph 정보 입력
+for _ in range(M):
+    a, b = map(int, input().split())  # 1,2
+    graph[a][b] = True
+    graph[b][a] = True
+
+# 2. bfs
+dfs(V)
+print()
+
+# 3. bfs
+visited = [False] * (N + 1)
+q = [V]
+bfs()
---
+++ 6weeks(12.23)/ReadMe.md
@@ -0,0 +1,7 @@
+# https://www.acmicpc.net/problem/1260
+
+- DFS, BFS 문제
+- 백준
+
+-> 왜 항상 dfs와 bfs문제는 풀면 그 다음에 알고리즘을 할 때 까먹을까..
+-> ㅜㅜ 슬프도다
